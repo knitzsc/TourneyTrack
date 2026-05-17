3673ca4e3c1619222388406512dd6088b5812797
unknown <knitzsc@purdue.edu>
2026-05-17 18:55:25 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold">
+        TourneyTrack
+      </h1>
+
+      <p className="mt-4">
+        Tournament management platform.
+      </p>
+    </main>
+  )
+}
